518da2608f8c2a37ff99088a410713f1262d2e23
sphejiahuan <iamtony_zhou@yahoo.com>
2022-12-26 23:08:32 -0600

    hash

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // const encrypt = require('mongoose-encryption');
-
+const md5 = require("md5");
 
 const app = express();
 
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
 
 // const secret = "process.env.SOME_LONG_UNGUESSABLE_STRING";
 const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret ,encryptedFields : ["password"]});
+// userSchema.plugin(encrypt, { secret: secret ,encryptedFields : ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,7 +45,7 @@ app.get('/register',function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email:req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -59,7 +59,7 @@ app.post("/register",function(req,res){
 
 app.post('/login',function(req,res){
     const username =req.body.username;
-    const password=  req.body.password;
+    const password=  md5(req.body.password);
 
     User.findOne({email : username},
         function(err,foundUser){
